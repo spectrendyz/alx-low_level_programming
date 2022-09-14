fc350ce1e2226a444134df09004a38f26677469b
spectrendyz <akpaudoetieneobong@gmail.com>
2022-09-14 15:34:39 +0000

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
-/**
- * print_alphabet - Prints the alphabet in lowercase
- *
- * Return: Always 0 (Success)
- */
 
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0.
+ */
 void print_alphabet(void)
 {
-	char alpha;
-
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	{
-		_putchar(alpha);
-	}
-	_putchar('\n');
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
