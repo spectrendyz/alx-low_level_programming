5019261e02bea72a7458da90a49b03ac656c70b7
spectrendyz <akpaudoetieneobong@gmail.com>
2022-09-15 21:03:56 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - This function print numbers
+ *
+ * return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
