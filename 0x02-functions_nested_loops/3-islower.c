b6e58bbefa00cf83b5d202b0460311146244cd11
spectrendyz <akpaudoetieneobong@gmail.com>
2022-09-16 08:31:41 +0000

    let go on

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include  "main.h"
+
+
+/**
+ * _islower - print_alphabet
+ * @c: its mine damnit
+ *
+ * Return: 1 if lowercase and 0 otherwise;
+ * Does my stuff explained.
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
