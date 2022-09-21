994295c9dbeb8c674386b8e6dee3b2d6aea831e6
spectrendyz <akpaudoetieneobong@gmail.com>
2022-09-21 08:50:55 +0000

    array strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcmp - This function compares two strings s1 and s2
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: Returns and integer less than, equal to or less than 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int _strcmp(char *s1, char *s2)
+	{
+		int i, status;
+	int i, status;
+
+		i = 0;
+		while (s1[i] != '\0' || s2[i] != '\0')
+		{
+			if (s1[i] != s2[i])
+			{
+				status = s1[i] - s2[i];
+				break;
+			}
+			else
+			{
+				return (0);
+			}
+		}
+		return (status);
+	}
+}
