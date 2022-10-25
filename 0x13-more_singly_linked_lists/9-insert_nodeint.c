efa263bfaca77c65f8e73dee8a3baccf3fc3561f
spectrendyz <akpaudoetieneobong@gmail.com>
2022-10-25 14:50:21 +0000

    welcome back

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
